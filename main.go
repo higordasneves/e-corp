21c6fe1fa57e53d4107882bc2e56c61a74b1dad2
Higor Neves <higordasneves@gmail.com>
2021-11-16 16:07:18 -0300

    chore: database configuration

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+	"github.com/higordasneves/e-corp/pkg/gateway/config"
+	_ "github.com/joho/godotenv/autoload"
+	_ "github.com/lib/pq"
+	"log"
+)
+
+var db *sql.DB
 
 func main() {
-	fmt.Println("Initial commit")
+	dbCfg := config.DatabaseConfig{}
+	dbCfg.LoadEnv()
+	dbDNS := dbCfg.DNS()
+
+	fmt.Println("Accessing database", dbDNS)
+	db, err := sql.Open(dbCfg.Driver, dbDNS)
+	if err != nil {
+		fmt.Println(err)
+		log.Fatal(config.ErrConnectDB, err)
+	}
+	defer db.Close()
+	if err = db.Ping(); err != nil {
+		fmt.Println(err)
+		log.Fatal(config.ErrConnectDB, err)
+	}
 }
---
+++ pkg/gateway/config/config.go
@@ -0,0 +1,41 @@
+package config
+
+import (
+	"errors"
+	"fmt"
+	"github.com/ilyakaznacheev/cleanenv"
+	"log"
+)
+
+var (
+	ErrConnectDB = errors.New("Failed to connect to database")
+)
+
+type DatabaseConfig struct {
+	Driver   string `env:"DB_DRIVER" env-default:"postgres"`
+	Host     string `env:"DB_HOST" env-default:"fullstack-postgres"`
+	Name     string `env:"DB_NAME" env-default:"ecorp"`
+	User     string `env:"DB_USER" env-default:"developer"`
+	Password string `env:"DB_PASSWORD" env-default:"123456"`
+	Port     string `env:"DB_PORT" env-default:"5432"`
+}
+
+//LoadEnv loads environment variables into a DatabaseConfig struct
+func (dbConfig *DatabaseConfig) LoadEnv() {
+	err := cleanenv.ReadEnv(dbConfig)
+	if err != nil {
+		log.Fatal("Fail, .env file not found")
+	}
+}
+
+//DNS returns database domain name servers
+func (dbConfig *DatabaseConfig) DNS() (dns string) {
+	dns = fmt.Sprintf("postgres://%v:%v@%v:%v/%v?sslmode=disable",
+		dbConfig.User,
+		dbConfig.Password,
+		dbConfig.Host,
+		dbConfig.Port,
+		dbConfig.Name,
+	)
+	return
+}
