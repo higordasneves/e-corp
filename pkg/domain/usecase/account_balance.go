8d535938f6ea92e15092d41bf3051764fa0c348e
higor <higordasneves@gmail.com>
2024-09-26 22:52:58 -0300

    chore: add godoc to GetBalance use case

--- pkg/domain/usecase/account_balance.go
+++ pkg/domain/usecase/account_balance.go
@@ -7,7 +7,8 @@ import (
 	"github.com/gofrs/uuid/v5"
 )
 
-// GetBalance returns a bank account balance
+// GetBalance returns the current balance of the account.
+// the repository return domain.ErrNotFound if the account not exists.
 func (accUseCase AccountUseCase) GetBalance(ctx context.Context, id uuid.UUID) (int, error) {
 	balance, err := accUseCase.R.GetBalance(ctx, id)
 	if err != nil {
