1a7590b46e9741ff2dfacd47417dedbc4ed59b0f
higor <higordasneves@gmail.com>
2024-09-26 22:59:39 -0300

    chore: refact secrets docs

--- pkg/domain/vos/secret.go
+++ pkg/domain/vos/secret.go
@@ -22,7 +22,8 @@ func (hashSecret Secret) String() string {
 	return string(hashSecret)
 }
 
-// NewSecret returns hash of password
+// NewSecret returns hash of password.
+// If the number of character is less than minSecretLength returns ErrSmallSecret.
 func NewSecret(s string) (Secret, error) {
 	if len(s) < minSecretLength {
 		return "", ErrSmallSecret
@@ -30,19 +31,21 @@ func NewSecret(s string) (Secret, error) {
 
 	hashSecret, err := bcrypt.GenerateFromPassword([]byte(s), 10)
 	if err != nil {
-		return "", err
+		return "", fmt.Errorf("unexpeted error generating secret: %w", err)
 	}
+
 	return Secret(hashSecret), nil
 }
 
 // CompareHashSecret compares password sent by user and stored password hash.
+// Returns ErrInvalidPass if the provided secret doesn't match with the hash password.
 func (hashSecret Secret) CompareHashSecret(secret string) error {
 	err := bcrypt.CompareHashAndPassword([]byte(hashSecret), []byte(secret))
 	if err != nil {
 		if errors.Is(err, bcrypt.ErrMismatchedHashAndPassword) {
 			return ErrInvalidPass
 		}
-		return err
+		return fmt.Errorf("unexpeted error comparing password: %w", err)
 	}
 
 	return nil
