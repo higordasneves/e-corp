658198fb42d5cb9edc018c0402b98320f4f14065
higor <higordasneves@gmail.com>
2024-10-13 09:19:48 -0300

    refact: controller tests

--- pkg/gateway/controller/account_list_test.go
+++ pkg/gateway/controller/account_list_test.go
@@ -22,7 +22,77 @@ import (
 	"github.com/higordasneves/e-corp/pkg/gateway/controller/reponses"
 )
 
-func TestAccountController_ListAccounts(t *testing.T) {
+func TestAccountController_ListAccounts_Success(t *testing.T) {
+	t.Parallel()
+
+	request := bytes.NewReader([]byte(`{"ids":["019282db-ff95-76cd-8b7f-c3a07b52a57c", "019282db-ff95-76ce-8ddd-ec5abceffa25"], "page_size":100, "page_token":""}`))
+	uc := &mocks.AccountUseCaseMock{
+		ListAccountsFunc: func(ctx context.Context, input usecase.ListAccountsInput) (usecase.ListAccountsOutput, error) {
+			assert.Equal(t, []uuid.UUID{uuid.FromStringOrNil("019282db-ff95-76cd-8b7f-c3a07b52a57c"), uuid.FromStringOrNil("019282db-ff95-76ce-8ddd-ec5abceffa25")}, input.IDs)
+			assert.Equal(t, 100, input.PageSize)
+
+			return usecase.ListAccountsOutput{
+				Accounts: []entities.Account{
+					{
+						ID:        uuid.FromStringOrNil("019282db-ff95-76cd-8b7f-c3a07b52a57c"),
+						Name:      "Elliot",
+						Document:  "55566677780",
+						Balance:   9700000,
+						CreatedAt: time.Date(2024, 1, 1, 0, 0, 0, 0, time.UTC),
+					},
+					{
+						ID:        uuid.FromStringOrNil("019282db-ff95-76ce-8ddd-ec5abceffa25"),
+						Name:      "Mr. Robot",
+						Document:  "55566677781",
+						Balance:   5596400,
+						CreatedAt: time.Date(2024, 1, 1, 0, 0, 0, 0, time.UTC),
+					},
+				},
+				NextPage: &usecase.ListAccountsInput{
+					LastFetchedID: uuid.FromStringOrNil("019282db-ff95-76d0-a96d-41f561a1af28"),
+					PageSize:      100,
+				},
+			}, nil
+		},
+	}
+	want := `{"accounts":[{"id":"019282db-ff95-76cd-8b7f-c3a07b52a57c","name":"Elliot","document":"55566677780","balance":9700000,"created_at":"2024-01-01T00:00:00Z"},{"id":"019282db-ff95-76ce-8ddd-ec5abceffa25","name":"Mr. Robot","document":"55566677781","balance":5596400,"created_at":"2024-01-01T00:00:00Z"}],"next_page":"eyJJRHMiOm51bGwsIkxhc3RGZXRjaGVkSUQiOiIwMTkyODJkYi1mZjk1LTc2ZDAtYTk2ZC00MWY1NjFhMWFmMjgiLCJQYWdlU2l6ZSI6MTAwfQ=="}`
+	accCtrl := controller.NewAccountController(uc, logTest)
+	router := mux.NewRouter()
+	router.HandleFunc("/accounts", accCtrl.ListAccounts).Methods(http.MethodPost)
+
+	req := httptest.NewRequest(http.MethodPost, "/accounts", request)
+	response := httptest.NewRecorder()
+
+	// execute
+	router.ServeHTTP(response, req)
+
+	// assert
+	assert.Equal(t, strings.TrimSpace(want), strings.TrimSpace(response.Body.String()))
+	assert.Equal(t, 200, response.Code)
+
+	// request with page token
+	request = bytes.NewReader([]byte(`{"page_token":"eyJJRHMiOm51bGwsIkxhc3RGZXRjaGVkSUQiOiIwMTkyODJkYi1mZjk1LTc2ZDAtYTk2ZC00MWY1NjFhMWFmMjgiLCJQYWdlU2l6ZSI6MTAwfQ=="}`))
+	uc = &mocks.AccountUseCaseMock{
+		ListAccountsFunc: func(ctx context.Context, input usecase.ListAccountsInput) (usecase.ListAccountsOutput, error) {
+			assert.Equal(t, usecase.ListAccountsInput{
+				LastFetchedID: uuid.FromStringOrNil("019282db-ff95-76d0-a96d-41f561a1af28"),
+				PageSize:      100,
+			}, input)
+			return usecase.ListAccountsOutput{}, nil
+		},
+	}
+	accCtrl = controller.NewAccountController(uc, logTest)
+	router = mux.NewRouter()
+	router.HandleFunc("/accounts", accCtrl.ListAccounts).Methods(http.MethodPost)
+
+	req = httptest.NewRequest(http.MethodPost, "/accounts", request)
+	response = httptest.NewRecorder()
+	// execute
+	router.ServeHTTP(response, req)
+	assert.Equal(t, http.StatusOK, response.Code)
+}
+
+func TestAccountController_ListAccounts_Failure(t *testing.T) {
 	t.Parallel()
 
 	type fields struct {
@@ -36,43 +106,6 @@ func TestAccountController_ListAccounts(t *testing.T) {
 		want         string
 		expectedCode int
 	}{
-		{
-			name:    "with success",
-			request: bytes.NewReader([]byte(`{"ids":["019282db-ff95-76cd-8b7f-c3a07b52a57c", "019282db-ff95-76ce-8ddd-ec5abceffa25"], "page_size":100, "page_token":""}`)),
-			fields: fields{
-				accUseCase: &mocks.AccountUseCaseMock{
-					ListAccountsFunc: func(ctx context.Context, input usecase.ListAccountsInput) (usecase.ListAccountsOutput, error) {
-						assert.Equal(t, []uuid.UUID{uuid.FromStringOrNil("019282db-ff95-76cd-8b7f-c3a07b52a57c"), uuid.FromStringOrNil("019282db-ff95-76ce-8ddd-ec5abceffa25")}, input.IDs)
-						assert.Equal(t, 100, input.PageSize)
-
-						return usecase.ListAccountsOutput{
-							Accounts: []entities.Account{
-								{
-									ID:        uuid.FromStringOrNil("019282db-ff95-76cd-8b7f-c3a07b52a57c"),
-									Name:      "Elliot",
-									Document:  "55566677780",
-									Balance:   9700000,
-									CreatedAt: time.Date(2024, 1, 1, 0, 0, 0, 0, time.UTC),
-								},
-								{
-									ID:        uuid.FromStringOrNil("019282db-ff95-76ce-8ddd-ec5abceffa25"),
-									Name:      "Mr. Robot",
-									Document:  "55566677781",
-									Balance:   5596400,
-									CreatedAt: time.Date(2024, 1, 1, 0, 0, 0, 0, time.UTC),
-								},
-							},
-							NextPage: &usecase.ListAccountsInput{
-								LastFetchedID: uuid.FromStringOrNil("019282db-ff95-76d0-a96d-41f561a1af28"),
-								PageSize:      100,
-							},
-						}, nil
-					},
-				},
-			},
-			want:         `{"accounts":[{"id":"019282db-ff95-76cd-8b7f-c3a07b52a57c","name":"Elliot","document":"55566677780","balance":9700000,"created_at":"2024-01-01T00:00:00Z"},{"id":"019282db-ff95-76ce-8ddd-ec5abceffa25","name":"Mr. Robot","document":"55566677781","balance":5596400,"created_at":"2024-01-01T00:00:00Z"}],"next_page":"eyJJRHMiOm51bGwsIkxhc3RGZXRjaGVkSUQiOiIwMTkyODJkYi1mZjk1LTc2ZDAtYTk2ZC00MWY1NjFhMWFmMjgiLCJQYWdlU2l6ZSI6MTAwfQ=="}`,
-			expectedCode: 200,
-		},
 		{
 			name:    "empty database",
 			request: bytes.NewReader([]byte(`{"ids":[], "page_size":100, "page_token":""}`)),
