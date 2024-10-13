ff2d2a0f4f2ff6679947641dda3f7f9d28cf330e
higor <higordasneves@gmail.com>
2024-10-13 12:38:35 -0300

    refact: TransferUseCase contract

--- pkg/gateway/controller/transfer.go
+++ pkg/gateway/controller/transfer.go
@@ -5,15 +5,14 @@ import (
 
 	"github.com/sirupsen/logrus"
 
-	"github.com/higordasneves/e-corp/pkg/domain/entities"
 	"github.com/higordasneves/e-corp/pkg/domain/usecase"
 )
 
 //go:generate moq -stub -pkg mocks -out mocks/transfers_uc.go . TransferUseCase
 
 type TransferUseCase interface {
-	Transfer(ctx context.Context, transferInput *usecase.TransferInput) (*entities.Transfer, error)
-	FetchTransfers(ctx context.Context, id string) ([]entities.Transfer, error)
+	ListAccountTransfers(ctx context.Context, input usecase.ListAccountTransfersInput) (usecase.ListAccountTransfersOutput, error)
+	Transfer(ctx context.Context, input usecase.TransferInput) (usecase.TransferOutput, error)
 }
 
 type TransferController struct {
